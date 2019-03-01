22454d959fa52832dab9f6170c5c8af3f094d62b
Ryan Murry <ryanjmurry@gmail.com>
2019-02-28 21:56:15 -0800

    create player model

---
+++ src/app/player.model.ts
@@ -0,0 +1,36 @@
+export class Player {
+  public mfsId: string;
+  public firstName: string;
+  public lastName: string;
+  public jerseyNumber?: string;
+  public img?: string;
+  public team: {
+    id?: string;
+    city?: string;
+    name?: string;
+  };
+  public overall: {
+    wins: number;
+    losses: number;
+  };
+  public rebounder: {
+    wins: number;
+    losses: number;
+  };
+  public defender: {
+    wins: number;
+    losses: number;
+  };
+  public shooter: {
+    wins: number;
+    losses: number;
+  };
+  public scorer: {
+    wins: number;
+    losses: number;
+  };
+  public passer: {
+    wins: number;
+    losses: number;
+  };
+}
