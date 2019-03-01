94e77011cedb522c7fdb0940af76bc0cab2375bb
Ryan Murry <ryanjmurry@gmail.com>
2019-02-28 21:56:02 -0800

    import material, angularfire, and angular firestore modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { RankerComponent } from './ranker/ranker.component';
+import { environment } from '../environments/environment';
+import { NbaService } from './services/nba.service';
+import { ErrorComponent } from './shared/components/error/error.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, RankerComponent, ErrorComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [NbaService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
