c0856e8f308277cc3b8796e7af8b312bdac73c1e
Ryan Murry <ryanjmurry@gmail.com>
2019-02-28 21:55:18 -0800

    add initial routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RankerComponent } from './ranker/ranker.component';
+import { ErrorComponent } from './shared/components/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/nba', pathMatch: 'full' },
+  { path: 'nba', component: RankerComponent },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
