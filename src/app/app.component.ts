1eaf4fdf9426996c96eebb4643ff23c0c0b65b55
Ryan Murry <ryanjmurry@gmail.com>
2019-03-05 20:37:20 -0800

    clean up app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ranker';
-}
+export class AppComponent {}
