49ed46eb38d2de9862e3d65334ccfef023f3adc0
Ryan Murry <ryanjmurry@gmail.com>
2019-03-10 15:42:15 -0700

    add logic for king of the hill

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -19,13 +19,17 @@ export class LeaderboardComponent implements AfterViewInit {
     'wins',
     'losses'
   ];
+  leader: Player;
+
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor(private nbaService: NbaService) {}
 
   ngAfterViewInit() {
+    this.loading = true;
     this.nbaService.getPlayerData().subscribe(data => {
       this.dataSource = new MatTableDataSource(data);
+      this.leader = this.dataSource.filteredData[0];
       this.dataSource.sort = this.sort;
       this.dataSource.paginator = this.paginator;
       this.loading = false;
@@ -37,6 +41,42 @@ export class LeaderboardComponent implements AfterViewInit {
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
+    }
+  }
+
+  getPlayerPosition(pos: string): string {
+    switch (pos) {
+      case 'PG':
+        return 'Point Guard';
+        break;
+      case 'SG':
+        return 'Shooting Guard';
+        break;
+      case 'G':
+        return 'Guard';
+        break;
+      case 'F':
+        return 'Forward';
+        break;
+      case 'SF':
+        return 'Small Forward';
+        break;
+      case 'PF':
+        return 'Power Forward';
+        break;
+      case 'C':
+        return 'Center';
+        break;
+      default:
+        break;
+    }
+  }
+
+  getJerseyNumber(player: Player): string {
+    if (player.jerseyNumber) {
+      return `#${player.jerseyNumber}`;
+    } else {
+      return;
     }
   }
 }
