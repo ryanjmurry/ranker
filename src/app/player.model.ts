3b1d8cfe15fa586dba75a6c77e1b3c0201176ea7
Ryan Murry <ryanjmurry@gmail.com>
2019-03-09 23:26:38 -0800

    update player model

--- src/app/player.model.ts
+++ src/app/player.model.ts
@@ -5,35 +5,12 @@ export class Player {
   public jerseyNumber?: string;
   public position: string;
   public img?: string;
-  public team: {
-    id?: string;
-    city?: string;
-    name?: string;
-    abbreviation?: string;
-    logo?: string;
-  };
-  public overall: {
-    wins: number;
-    losses: number;
-  };
-  public rebounder: {
-    wins: number;
-    losses: number;
-  };
-  public defender: {
-    wins: number;
-    losses: number;
-  };
-  public shooter: {
-    wins: number;
-    losses: number;
-  };
-  public scorer: {
-    wins: number;
-    losses: number;
-  };
-  public passer: {
-    wins: number;
-    losses: number;
-  };
+  public teamId?: string;
+  public teamCity?: string;
+  public teamName?: string;
+  public teamAbbreviation?: string;
+  public teamLogo?: string;
+  public wins: number;
+  public losses?: number;
+  public winPercentage: number;
 }
