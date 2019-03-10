1f1c8a6f0c5273adab22060033e3ea37aad5168a
Ryan Murry <ryanjmurry@gmail.com>
2019-03-09 23:27:31 -0800

    add leaderboard with paginating, sorting, and filtering

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+import { NbaService } from '../services/nba.service';
+import { Player } from '../player.model';
 
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
   styleUrls: ['./leaderboard.component.scss']
 })
-export class LeaderboardComponent implements OnInit {
+export class LeaderboardComponent implements AfterViewInit {
+  loading: boolean = true;
+  dataSource: MatTableDataSource<Player>;
+  displayedColumns: string[] = [
+    'teamAbbreviation',
+    'firstName',
+    'lastName',
+    'winPercentage',
+    'wins',
+    'losses'
+  ];
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  constructor(private nbaService: NbaService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.nbaService.getPlayerData().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+      this.loading = false;
+    });
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
