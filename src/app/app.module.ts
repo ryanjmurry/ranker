cdae1a73029dfbfa40b649fbbc232c2aa233bad7
Ryan Murry <ryanjmurry@gmail.com>
2019-03-10 14:41:24 -0700

    add dialog explaining site; store boolean in local storage to prevent additional dialogs showing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,18 +13,28 @@ import { NbaService } from './services/nba.service';
 import { ErrorComponent } from './shared/components/error/error.component';
 import { HomeComponent } from './home/home.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { InstructionsDialogComponent } from './shared/dialogs/instructions-dialog/instructions-dialog.component';
 
 @NgModule({
-  declarations: [AppComponent, RankerComponent, ErrorComponent, HomeComponent, LeaderboardComponent],
+  declarations: [
+    AppComponent,
+    RankerComponent,
+    ErrorComponent,
+    HomeComponent,
+    LeaderboardComponent,
+    InstructionsDialogComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
     MaterialModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule
   ],
   providers: [NbaService],
+  entryComponents: [InstructionsDialogComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/shared/dialogs/instructions-dialog/instructions-dialog.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InstructionsDialogComponent } from './instructions-dialog.component';
+
+describe('InstructionsDialogComponent', () => {
+  let component: InstructionsDialogComponent;
+  let fixture: ComponentFixture<InstructionsDialogComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ InstructionsDialogComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InstructionsDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
