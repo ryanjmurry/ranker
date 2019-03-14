91994081b8363518ebf73c911d1a8fe85a61c879
Ryan Murry <ryanjmurry@gmail.com>
2019-03-13 22:58:30 -0700

    add production environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBj4BK3CH8MkRevR7DN40yrO4SIcZxMQac',
+    authDomain: 'ranker-app.firebaseapp.com',
+    databaseURL: 'https://ranker-app.firebaseio.com',
+    projectId: 'ranker-app',
+    storageBucket: 'ranker-app.appspot.com',
+    messagingSenderId: '580090043702'
+  }
 };
